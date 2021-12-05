c9aae8d677f3cb791ed3068c0c459b98f4ab8410
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-05 22:16:42 +0545

    UserDataDisplay

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 
 import * as React from 'react';
 
+import { dequal } from 'dequal';
+
+import * as userClient from './user-client';
 import { useAuth } from './auth-context';
 
 const UserContext = React.createContext();
@@ -68,6 +71,99 @@ function useUser() {
   return context;
 }
 
+function UserSettings() {
+  const [{ user, status, error }, userDispatch] = useUser();
+
+  const isPending = status === 'pending';
+  const isRejected = status === 'rejected';
+
+  const [formState, setFormState] = React.useState(user);
+
+  const isChanged = !dequal(user, formState);
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    userDispatch({ type: 'start update', updates: formState });
+    userClient.updateUser(user, formState).then(
+      (updatedUser) => userDispatch({ type: 'finish update', updatedUser }),
+      (error) => userDispatch({ type: 'fail update', error })
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div style={{ marginBottom: 12 }}>
+        <label style={{ display: 'block' }} htmlFor='username'>
+          Username
+        </label>
+        <input
+          id='username'
+          name='username'
+          disabled
+          readOnly
+          value={formState.username}
+          style={{ width: '100%' }}
+        />
+      </div>
+      <div style={{ marginBottom: 12 }}>
+        <label style={{ display: 'block' }} htmlFor='tagline'>
+          Tagline
+        </label>
+        <input
+          id='tagline'
+          name='tagline'
+          value={formState.tagline}
+          onChange={handleChange}
+          style={{ width: '100%' }}
+        />
+      </div>
+      <div style={{ marginBottom: 12 }}>
+        <label style={{ display: 'block' }} htmlFor='bio'>
+          Biography
+        </label>
+        <textarea
+          id='bio'
+          name='bio'
+          value={formState.bio}
+          onChange={handleChange}
+          style={{ width: '100%' }}
+        />
+      </div>
+      <div>
+        <button
+          type='button'
+          onClick={() => {
+            setFormState(user);
+            userDispatch({ type: 'reset' });
+          }}
+          disabled={!isChanged || isPending}
+        >
+          Reset
+        </button>
+        <button
+          type='submit'
+          disabled={(!isChanged && !isRejected) || isPending}
+        >
+          {isPending
+            ? '...'
+            : isRejected
+            ? '✖ Try again'
+            : isChanged
+            ? 'Submit'
+            : '✔'}
+        </button>
+        {isRejected ? (
+          <pre style={{ color: 'red' }}>{error.message}</pre>
+        ) : null}
+      </div>
+    </form>
+  );
+}
+
 function UserDataDisplay() {
   const [{ user }] = useUser();
   return <pre>{JSON.stringify(user, null, 2)}</pre>;
@@ -85,6 +181,7 @@ function App() {
       }}
     >
       <UserProvider>
+        <UserSettings />
         <UserDataDisplay />
       </UserProvider>
     </div>
