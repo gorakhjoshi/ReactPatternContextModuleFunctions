6fd13d87fca60c9179b871cb2456255928899ecd
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-05 22:17:41 +0545

    instructions added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // Context Module Functions
 
 import * as React from 'react';
-
 import { dequal } from 'dequal';
 
-import * as userClient from './user-client';
-import { useAuth } from './auth-context';
+// ./context/user-context.js
+
+import * as userClient from '../user-client';
+import { useAuth } from '../auth-context';
 
 const UserContext = React.createContext();
 UserContext.displayName = 'UserContext';
@@ -71,6 +72,14 @@ function useUser() {
   return context;
 }
 
+// 🐨 add a function here called `updateUser`
+// Then go down to the `handleSubmit` from `UserSettings` and put that logic in
+// this function. It should accept: dispatch, user, and updates
+
+// export {UserProvider, useUser}
+
+// src/screens/user-profile.js
+// import {UserProvider, useUser} from './context/user-context'
 function UserSettings() {
   const [{ user, status, error }, userDispatch] = useUser();
 
@@ -87,6 +96,7 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    // 🐨 move the following logic to the `updateUser` function you create above
     userDispatch({ type: 'start update', updates: formState });
     userClient.updateUser(user, formState).then(
       (updatedUser) => userDispatch({ type: 'finish update', updatedUser }),
